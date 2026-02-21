93e3bcb9273e75ab23878db7f7e8041724e4622e
Ceferino Jumao-as V <ceferinojumaoas29@gmail.com>
2026-02-21 16:07:48 +0800

    style(ui): Fix add task button & column cards drag colors

--- pages/dashboard/dashboard-dom.js
+++ pages/dashboard/dashboard-dom.js
@@ -6,7 +6,7 @@ export const DashboardDOM = {
         $btn.type = 'button';
         $btn.id = 'add-task-btn';
         $btn.className = 'btn btn--primary';
-        $btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" /></svg><span>Add Task</span>';
+        $btn.innerHTML = '<span class="btn__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" /></svg></span><span class="btn__label">Add Task</span>';
         return $btn;
     },
 
